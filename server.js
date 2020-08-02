15cb8666785bdfc8e64bd5df0004eb0d78357bd8
Mohand Abdalrahman <mohand.abdalrahman@mondia.com>
2020-08-02 14:19:03 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const path = require('path')
 const cors = require('cors')
 dotenv.config({ path: './config.env' })
 const app = express()
@@ -21,5 +22,15 @@ app.use('/api/v1/users', require('./routes/user'))
 app.use('/api/v1/profile', require('./routes/profile'))
 app.use('/api/v1/auth', require('./routes/auth'))
 app.use('/api/v1/posts', require('./routes/posts'))
+
+//SERVE STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(process.env.PORT, () => console.log(`App is listen on PORT : ${process.env.PORT}`))
